3dea961a54c78c794bb8a0e39c9c80b9e924767d
rhys_e <rhys.ewins@live.com.au>
2023-11-27 19:39:54 +0800

    buffer as base64

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const PORT = process.env.port || 4000;
 app.use(bodyParser.json());
 
 app.post('/segment', async (req, res) => {
-    const { url, timestamp } = req.body;
+    const { url, timestamp } = req.body;    
 
     try {
         const segmentBuffer = await segmentAudio(url, timestamp);
-        res.setHeader('Content-Type', 'audio/mpeg');
-        res.send(segmentBuffer);
+        const base64Audio = segmentBuffer.toString('base64');
+        res.setHeader('Content-Type', 'text/plain');
+        res.send(base64Audio);
     } catch (error) {
         console.error('Error processing audio segment:', error);
         res.status(500).send('Error processing audio segment');
